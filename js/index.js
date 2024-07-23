f74b3c7b72b28d5305f9e651b1abdc73d4c2d9b2
Amro6779 <mr487680@gmail.com>
2024-07-23 21:28:34 +0300

    index.js

--- js/index.js
+++ js/index.js
@@ -5,28 +5,20 @@ import { getMovieAttr, navGetSection, scroll, topZero } from "./scroll.js";
 validations();
 scroll();
 getMovie("movie/now_playing");
-$('.nav-menu').on('click' , function () {
-    openNav();
-});
-
-$('.menu a').on('click' , function () {
-    getMovieAttr();
-});
-
-$('.menu li a').on('click' , function () {
-    navGetSection();
-});
-
-$('#back-to-top').on('click' , function () {
-    topZero();
-});
-
+$('.nav-menu').click(openNav);
+$('.menu a').click(getMovieAttr);
+$('.menu li a').click(navGetSection);
+$("#back-to-top").click(topZero);
 $('#search').on("input", e => {
     searchMovie(e.target.value);
     if(e.target.value == "")
     {
         getMovie("movie/now_playing");
     }
+});
+$(document).ready(function(){
+    $('.loading').fadeOut(2000)
+});
 });
 
 $(document).ready(function(){
